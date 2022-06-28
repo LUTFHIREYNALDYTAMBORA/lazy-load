c3396f264abe0e51af5c6317cbfe2e5b5d9438c3
LUTFHI REYNALDY TAMBORA <lutfhiitamboraa@gmail.com>
2022-06-28 23:44:08 +0700

    lazy-load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import './App.css';
 
 function App() {
+  const userID = 'dLhcDBAcGu6SxucaVETV3dfdZiGi1SghBdfsvr-4A2c';
+  const url = `https://api.unsplash.com/photos/random?client_id=${userID}&count=30`;
+  const [images, setImages] = useState([]);
+
+  const getImages = () => {
+    Axios.get(url).then((res) => {
+      setImages(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getImages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!images) {
+    return <h2>Loading.....</h2>
+  }
+  console.log(images);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(images || []).map((val) => {
+        return (
+          <LazyLoadImage
+            alt={val?.alt_description}
+            effect="blur"
+            height="500px"
+            key={val.id}
+            loading="lazy"
+            src={val?.urls?.regular}
+            width="400px"
+          />
+        );
+      }
+      )}
     </div>
   );
 }
